b408473bbd4403a8e6c5cc19520e6c8b03e0a136
Tyler Young <tyler@tylerayoung.com>
2023-07-21 06:40:58 -0500

    Require the host application to start us

--- lib/okay_weather.ex
+++ lib/okay_weather.ex
@@ -12,5 +12,49 @@ defmodule OkayWeather do
   Now, on the other hand, the thing that makes this library okay in a *positive* sense: it's free! Weather data can get expensive, especially at scale. NOAA's data is free, and if you can cache the results, you can get good enough data without breaking the bank.
 
   **Warning**: okay_weather is pre-alpha right now. Please assume everything is broken.
+
+  ## Installation
+
+  1. Add the package to your `mix.exs`:
+    ```elixir
+    defp deps do
+    [
+      {:okay_weather, github: "s3cur3/okay_weather"},
+    ]
+    end
+    ```
+  2. Add `OkayWeather` to your `applications` list in `mix.exs`:
+    ```elixir
+    def application do
+    [
+      extra_applications: [:logger, :okay_weather],
+    ]
+    end
+    ```
+  3. Add `OkayWeather` to your supervision tree in your `application.ex`:
+    ```elixir
+    def start(_type, _args) do
+      children = [
+        OkayWeather.child_spec(),
+      ]
+      ...
+    end
+    ```
+  4. Run `mix deps.get`
   """
+  alias OkayWeather.AutoUpdatingCache.State
+
+  @spec child_spec([{:update_interval_ms, pos_integer()}]) :: Supervisor.child_spec()
+  def child_spec(opts \\ [update_interval_ms: :timer.minutes(5)]) do
+    update_timeout = Keyword.get(opts, :update_interval_ms, :timer.minutes(5))
+    cache_spec(:metar, &OkayWeather.UrlGen.metar/1, &OkayWeather.Metar.parse/1, update_timeout)
+  end
+
+  @spec cache_spec(atom, State.url_generator(), State.transform(), pos_integer()) ::
+          Supervisor.child_spec()
+  defp cache_spec(name, url_generator, transform, update_timeout)
+       when is_integer(update_timeout) do
+    args = [name, url_generator, transform, update_timeout]
+    %{id: name, start: {OkayWeather.AutoUpdatingCache, :start_link, args}}
+  end
 end
--- lib/okay_weather/application.ex
+++ lib/okay_weather/application.ex
@@ -9,19 +9,10 @@ defmodule OkayWeather.Application do
 
     children = [
       {Task.Supervisor, name: OkayWeather.FetchSupervisor},
-      {Registry, keys: :unique, name: OkayWeather.AutoUpdatingCache},
-      cache_spec(:metar, &OkayWeather.UrlGen.metar/1, &OkayWeather.Metar.parse/1)
+      {Registry, keys: :unique, name: OkayWeather.AutoUpdatingCache}
     ]
 
     opts = [strategy: :one_for_one, name: OkayWeather.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp cache_spec(name, url_generator, transform)
-       when is_atom(name) and is_function(url_generator, 1) and is_function(transform, 1) do
-    %{
-      id: name,
-      start: {OkayWeather.AutoUpdatingCache, :start_link, [name, url_generator, transform]}
-    }
   end
 end
