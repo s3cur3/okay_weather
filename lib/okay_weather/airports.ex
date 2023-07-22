1c9bbff2a3d303fa709035b00ed168b80da3e5c8
Tyler Young <tyler@tylerayoung.com>
2023-07-21 20:02:49 -0500

    [WIP] Nearest airport

--- lib/okay_weather/airports.ex
+++ lib/okay_weather/airports.ex
@@ -6,10 +6,27 @@ defmodule OkayWeather.Airports do
   @external_resource @csv_file_path
   @airports OkayWeather.Airports.Parser.parse_csv(@csv_file_path)
 
+  @airports_by_lon_lat @airports
+                       |> Map.values()
+                       |> Enum.group_by(&{floor(&1.longitude), floor(&1.latitude)})
+
+  @spec lon_lat(String.t()) :: {:ok, {float(), float()}} | {:error, :unknown_airport}
   def lon_lat(airport_code) do
-    case @airports[airport_code] do
+    case airports()[airport_code] do
       nil -> {:error, :unknown_airport}
       %Airport{latitude: lat, longitude: lon} -> {:ok, {lon, lat}}
     end
   end
+
+  def nearest_to_lon_lat(lon, lat) do
+  end
+
+  @spec in_lon_lat_bucket(float(), float()) :: [Airport.t()]
+  def in_lon_lat_bucket(lon, lat) do
+    lon_bucket = floor(lon)
+    lat_bucket = floor(lat)
+    @airports_by_lon_lat[{lon_bucket, lat_bucket}]
+  end
+
+  defp airports, do: @airports
 end
--- test/okay_weather/airports_test.exs
+++ test/okay_weather/airports_test.exs
@@ -7,4 +7,15 @@ defmodule OkayWeather.AirportsTest do
     assert Airports.lon_lat("00A") == {:ok, {-74.93360137939450, 40.07080078125}}
     assert Airports.lon_lat("ZZZZ") == {:ok, {130.270556, 30.784722}}
   end
+
+  test "buckets airports by lon/lat" do
+    airports_in_bucket = Airports.in_lon_lat_bucket(-122.3, 47.4)
+    assert length(airports_in_bucket) > 100
+    assert length(airports_in_bucket) < 1_000
+
+    codes_in_bucket = Enum.map(airports_in_bucket, & &1.airport_code)
+    assert "KSEA" in codes_in_bucket
+    assert "KGRF" in codes_in_bucket
+    assert "KBFI" in codes_in_bucket
+  end
 end
